817086ab85ca23cf20fd7ddc5554b2824d2764ea
simonoded <simonoded@gmail.com>
2013-05-04 21:07:28 +1000

    add puppet config for: basic nginx basic unicorn simple-sinatra-app

---
+++ puppet/unicorn/manifests/init.pp
@@ -0,0 +1,17 @@
+# == Class: unicorn install unicorn gem package
+# to add OS please edit the allowed OS
+class unicorn {
+    case $::operatingsystem {
+        'Ubuntu': {
+            $packages = 'ruby-bundler'
+         }
+         default: { fail('Unrecognized operating system') }
+    }
+    package {
+        'unicorn':
+            ensure      =>  latest,
+            provider    =>  gem;
+        $packages:
+            ensure      =>  latest;
+    }
+}
